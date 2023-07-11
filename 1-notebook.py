4727841f4ce3f70ea03e90b828a24c16ab8d796b
agry3636 <139188933+agry3636@users.noreply.github.com>
2023-07-11 17:35:42 -0600

    Add files via upload
    Python file called 1-notebook

---
+++ 1-notebook.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### *Exercise 2: Create markdown cell with title of the notebook*
+
+# # Data Science Tools and Ecosystem
+
+# ### *Exercise 3 - Create a markdown cell for an introduction*
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized
+
+# ## **Objectives:**
+
+# List popular languages for Data Science:
+# - Pyhton
+# - R
+# - C++
+# - Plotly
+# - Numpy
+
+# ### *Exercise 4 - Create a markdown cell to list data science languages*
+
+# #### Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. C++
+
+# ### *Exercise 5 - Create a markdown cell to list data science libraries*
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+
+# 1. Pandas
+# 2. GGplot2
+# 3. Lattice
+
+# ### *Exercise 6 - Create a markdown cell with a table of Data Science tools*
+
+# #### Table of tools
+
+# | Table of Tools |
+# |----------------|
+# | Apache Sparks  |
+# | TensorFlow     |
+# | RStudio        |
+# |----------------|
+
+# ### *Exercise 7 - Create a markdown cell introducing arithmetic expression examples*
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[6]:
+
+
+45*8
+
+
+# In[7]:
+
+
+76**2
+
+
+# In[8]:
+
+
+4-4
+
+
+# ### *Exercise 8 - Create a code cell to multiply and add numbers*
+
+# In[9]:
+
+
+##  This a simple arithmetic expression to mutiply then add integers.
+
+
+# In[10]:
+
+
+(3*4)+5
+
+
+# ### *Exercise 9 - Create a code cell to convert minutes to hours*
+
+# In[11]:
+
+
+## This will convert 200 minutes to hours by diving by 60.
+
+
+# In[12]:
+
+
+200//60
+
+
+# ### Exercise 11 - Create a markdown cell to indicate the Author's name
+
+# ## Author
+
+# Cindy Adriana Artavia
+
+# In[ ]:
+
+
+
+
